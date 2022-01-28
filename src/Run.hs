acec6650ac85678067fbade6747ef9a9a12a3c37
Mulleners <n.mulleners@uu.nl>
2022-01-28 11:02:41 +0100

    Add map sketch with two holes

--- src/Run.hs
+++ src/Run.hs
@@ -16,21 +16,40 @@ mapDef = Def
     }
   }
 
-run :: RIO App ()
-run = do
-  let def = mapDef
+-- A slightly more refined definition of map
+mapDef2 :: Def
+mapDef2 = Def
+  { name = "map"
+  , sig = TArr (TArr "a" "b") (TArr (list "a") (list "b"))
+  , body = Sketch
+    { expr = ELam "f" (TArr "a" "b")
+      $ EApp (EApp (EVar (Left "foldr")) (EHole 0)) (EHole 1)
+    , goals = Map.fromList
+      [ (0, TArr "a" (TArr (list "b") (list "b")))
+      , (1, list "b")
+      ]
+    }
+  }
+
+runSyn :: Env -> Def -> RIO App ()
+runSyn env def = do
   logInfo "Sketch:"
   logInfo ""
   logInfo <=< displayWithColor . indent 2 $ pretty def
   logInfo ""
   logInfo "Possible refinements:"
   logInfo ""
-  let syn = synthesize prelude (body def)
+  let syn = synthesize env (body def)
   let xss = takeWhile (not . null) . zip [0 :: Int ..] $ syn
   forM_ xss \(i, xs) -> do
     logInfo $ "Step: " <> fromString (show i)
     forM_ xs \x -> do
       d <- displayWithColor . expr $ sketch x
       logInfo ("  " <> d)
+
+run :: RIO App ()
+run = do
+  runSyn prelude mapDef
+  runSyn (Map.delete "foldr" prelude) mapDef2
   logInfo ""
   logInfo "Finished!"
