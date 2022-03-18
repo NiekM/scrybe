d9d53f0a8d495a5ad818f5bbe1f8359ad80a6ee1
Mulleners <n.mulleners@uu.nl>
2022-03-18 13:22:28 +0100

    Implement a minimal lazy evaluator

--- src/Language/Eval.hs
+++ src/Language/Eval.hs
@@ -3,8 +3,11 @@ module Language.Eval where
 
 import Import
 import Language.Syntax
+import RIO.List (splitAt, maximumMaybe)
 import qualified RIO.Map as Map
 import Control.Monad.State
+import Prettyprinter
+import Language.Parser
 
 class HasEnv a where
   env :: Lens' a (Map Var (Term Void))
@@ -57,3 +60,97 @@ match p e = case p of
   -- Note: pattern matching does not check for duplicate variables in a pattern
   App f x | App g y <- e -> liftM2 (<>) (match f g) (match x y)
   _ -> fail "Pattern match failed"
+
+type Address = Int
+type Body = Expr 'Term Var Void
+
+data Node
+  = NApp Address Address
+  | NFun Var [Var] Body
+  | NCtr Ctr
+  deriving (Eq, Ord, Show)
+
+type Stack = [Address]
+type Heap = Map Address Node
+type Global = Map Var Address
+type Def = (Var, [Var], Body)
+
+data GraphState = GraphState
+  { stack :: [Address]
+  , heap :: Map Address Node
+  , global :: Map Var Address
+  } deriving (Eq, Ord, Show)
+
+step_ :: GraphState -> Either GraphState GraphState
+step_ g@GraphState { stack, heap, global } = case stack of
+  [] -> Left g
+  (a:s) -> case Map.lookup a heap of
+    Nothing -> Left g
+    Just x -> case x of
+      NApp a1 a2 -> return $ GraphState (a1:a2:s) heap global
+      NFun _ xs e
+        | n <- length xs
+        , (as, s') <- splitAt n s
+        , length as == n ->
+          let (h, b) = inst e heap (Map.fromList (zip xs as) <> global)
+          in return $ GraphState (b:s') h global
+        | otherwise -> Left g
+      NCtr {} -> Left g
+
+alloc :: Heap -> Node -> (Heap, Address)
+alloc h n = (Map.insert i n h, i) where
+  i = case maximumMaybe (Map.keysSet h) of
+    Nothing -> 0
+    Just j -> j + 1
+
+inst :: Body -> Heap -> Global -> (Heap, Address)
+inst e h g = case e of
+  Hole i -> absurd i
+  App e1 e2 ->
+    let (h1, a1) = inst e1 h g
+        (h2, a2) = inst e2 h1 g
+    in alloc h2 (NApp a1 a2)
+  Var a -> (h, fromMaybe (error "Oh oh") $ Map.lookup a g)
+  Ctr c -> alloc h (NCtr c)
+  _ -> undefined
+
+initialHeap :: [Def] -> (Heap, Global)
+initialHeap = foldr (\x (h, g) -> allocate x h <> (h, g)) mempty
+
+compile :: [Def] -> GraphState
+compile program = GraphState { stack, heap, global } where
+  stack = [Map.findWithDefault (error "Oh no") "main" global]
+  (heap, global) = initialHeap program
+
+fromAddress :: Heap -> Address -> Maybe (Term Void)
+fromAddress h i = Map.lookup i h >>= \case
+  NApp x y -> App <$> fromAddress h x <*> fromAddress h y
+  NFun f _ _ -> return $ Var f
+  NCtr c -> return $ Ctr c
+
+allocate :: Def -> Heap -> (Heap, Global)
+allocate (name, args, body) heap = (heap', Map.singleton name address) where
+  (heap', address) = alloc heap (NFun name args body)
+
+toExpr :: GraphState -> NonEmpty (Term Void)
+toExpr GraphState { stack, heap } =
+  case stack <&> fromMaybe undefined . fromAddress heap of
+    [] -> error "Mmm"
+    x:xs -> x :| xs
+
+visualize :: ([GraphState], GraphState) -> Doc ann
+visualize (xs, y) = pretty (as, b) where
+  as = xs <&> \x ->
+    let e:|es = toExpr x
+    in apps (Hole e :| fmap (over holes absurd) es)
+  b = apps (toExpr y)
+
+defs :: [Def]
+defs =
+  [ ("main", [], parseUnsafe parser "fix twice")
+  , ("twice", ["a"], parseUnsafe parser "Pair a a")
+  , ("fix", ["f"], parseUnsafe parser "f (fix f)")
+  ]
+
+steps :: GraphState -> ([GraphState], GraphState)
+steps s = (s:) `first` either ([],) steps (step_ s)
