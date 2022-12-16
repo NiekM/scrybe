2a3ba1522ab9cb72dfdef73ebd2015f8506a581d
Mulleners <n.mulleners@uu.nl>
2022-12-16 17:00:54 +0100

    Cleanup Import

--- src/Import.hs
+++ src/Import.hs
@@ -12,30 +12,18 @@ module Import
   , module Utils.Type
   , Pretty(..), Doc, (<+>)
   , Dist
-  , Logic(..)
-  , BoundedLattice(..)
   , Unit(Unit)
   , Possibly(..), possibly
   , unsnoc
-  , maximumDef
   , mfold
   , failMaybe
-  , liftRWST
-  , evalState
-  , readerState
-  , readerStateMaybe
-  , readerStateT
-  , distr
   ) where
 
 import RIO hiding (local, assert, and, or)
-import RIO.List
 import qualified RIO.Map as Map
 import qualified RIO.Set as Set
 import Prettyprinter
 import Control.Monad.RWS hiding (local, All)
-import Control.Monad.Reader
-import Control.Monad.State
 import Lens.Micro.Platform
 import Data.Functor.Compose
 import Data.Monus.Dist (Dist)
@@ -91,9 +79,6 @@ unsnoc :: NonEmpty a -> ([a], a)
 unsnoc (x :| []) = ([], x)
 unsnoc (x :| y:ys) = (x:) `first` unsnoc (y :| ys)
 
-maximumDef :: (Ord a, Foldable f) => a -> f a -> a
-maximumDef d = fromMaybe d . maximumMaybe
-
 mfold :: (Foldable f, MonadPlus m) => f a -> m a
 mfold = msum . fmap return . toList
 
@@ -101,35 +86,3 @@ failMaybe :: MonadFail m => Maybe a -> m a
 failMaybe = \case
   Nothing -> fail ""
   Just x -> return x
-
-liftRWST :: (Monad m, Monoid w) => m a -> RWST r w s m a
-liftRWST m = RWST \_r s -> (,s,mempty) <$> m
-
-readerState :: State s (Reader s a) -> Reader s a
-readerState st = do
-  (x, s) <- asks $ runState st
-  local (const s) x
-
-readerStateT :: Monad m => StateT s m (ReaderT s m a) -> ReaderT s m a
-readerStateT st = do
-  x <- asks $ runStateT st
-  (y, s) <- ReaderT $ const x
-  local (const s) y
-
-readerStateMaybe :: StateT s Maybe (Reader s a) -> Reader s (Maybe a)
-readerStateMaybe st = do
-  x <- asks $ runStateT st
-  case x of
-    Nothing -> return Nothing
-    Just (y, s) -> local (const s) (return <$> y)
-
-distr :: MonadPlus m => Int -> Int -> m [Int]
-distr _ k | k <= 0 = mzero
-distr n 1 = return [n]
-distr n k = case compare n k of
-  LT -> mzero
-  EQ -> return $ replicate k 1
-  GT -> do
-    x <- mfold [1 .. n - k + 1]
-    xs <- distr (n - x) (k - 1)
-    return $ x : xs
