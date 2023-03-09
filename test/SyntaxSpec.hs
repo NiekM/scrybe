3c28305ee5eed40aa63e42c416f19ec300ff9bd0
Mulleners <n.mulleners@uu.nl>
2023-03-09 23:05:09 +0100

    Add QuickCheck tests

--- test/SyntaxSpec.hs
+++ test/SyntaxSpec.hs
@@ -1,7 +1,9 @@
 module SyntaxSpec where
 
 import Import
-import Test.Hspec
+import Test.Hspec hiding (Example)
+import Test.Hspec.QuickCheck
+import Test.QuickCheck hiding (Result)
 import Language.Defs
 import Language.Syntax
 import Language.Parser
@@ -10,24 +12,59 @@ import Prettyprinter
 import Prettyprinter.Render.Text
 import qualified RIO.Map as Map
 
+instance Arbitrary Value where
+  arbitrary = do
+    n <- frequency [(5, return 0), (2, return 1), (1, return 2)]
+    apps
+      <$> (Ctr . MkCtr . (<> fromString (show n)) <$> elements ["A", "B", "C"])
+      <*> vector n
+
+instance Arbitrary Example where
+  arbitrary = do
+    n <- chooseInt (0, 3)
+    lams <$> vector n <*> output
+    where
+      output = do
+        n <- frequency [(5, return 0), (2, return 1), (1, return 2)]
+        apps
+          <$> (Ctr . MkCtr . (<> fromString (show n)) <$>
+            elements ["A", "B", "C"])
+          <*> vectorOf n (oneof [output, return $ Hole Unit])
+
 roundtrip :: (Pretty a, Parse a, Eq a) => a -> Bool
 roundtrip x = case lexParse parser t of
   Just y -> x == y
   Nothing -> False
   where t = renderStrict . layoutPretty (LayoutOptions Unbounded) . pretty $ x
 
+prelude :: Spec
+prelude = do
+  f <- runIO $ readFileUtf8 "data/prelude.hs"
+  case lexParse parser f of
+    Nothing -> it "parses" False
+    Just x -> do
+      it "roundtrips" $ roundtrip x
+      let es = Map.fromList $ bindings x <&>
+            \(MkBinding a e) -> (a, over holes (absurd @Unit) e)
+      let ts = Map.fromList $ signatures x <&> \(MkSignature a t) -> (a, t)
+      let y = Map.assocs $ Map.intersectionWith (,) es ts
+      let m = fromDefs x
+      forM_ y \(v, (e, t)) -> describe (show (pretty v)) do
+        it "type checks" . isJust $ evalInfer (check mempty e t) 0 m
+
+value :: Spec
+value = do
+  describe "roundtrips via" do
+    prop "term" \v -> downcast (upcast @('Term Void) v) == Just v
+    prop "sketch" \v -> downcast (upcast @('Term Unit) v) == Just v
+    prop "result" \v -> downcast (upcast @'Det v) == Just v
+    prop "example" \v -> downcast (upcast @'Example v) == Just v
+
+ex :: Spec
+ex = prop "roundtrips via ex" \x -> fromEx (toEx x) == [x]
+
 spec :: Spec
 spec = do
-  describe "prelude" do
-    f <- runIO $ readFileUtf8 "data/prelude.hs"
-    case lexParse parser f of
-      Nothing -> it "parses" False
-      Just x -> do
-        it "roundtrips" $ roundtrip x
-        let es = Map.fromList $ bindings x <&>
-              \(MkBinding a e) -> (a, over holes (absurd @Unit) e)
-        let ts = Map.fromList $ signatures x <&> \(MkSignature a t) -> (a, t)
-        let y = Map.assocs $ Map.intersectionWith (,) es ts
-        let m = fromDefs x
-        forM_ y \(v, (e, t)) -> describe (show (pretty v)) do
-          it "type checks" . isJust $ evalInfer (check mempty e t) 0 m
+  describe "prelude" prelude
+  describe "value" value
+  describe "example" ex
---
+++ test/TypeSpec.hs
@@ -0,0 +1,44 @@
+module TypeSpec where
+
+import Import
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Language.Defs
+import Language.Syntax
+import Language.Parser
+import Language.Type
+import qualified RIO.Map as Map
+
+instance Arbitrary Mono where
+  arbitrary = do
+    n <- chooseInt (1, 3)
+    arrs <$> vectorOf n arg
+    where
+      arg = do
+        n <- frequency [(5, return 0), (2, return 1), (1, return 2)]
+        apps
+          <$> oneof
+            [ Ctr . MkCtr . (<> fromString (show n)) <$>
+              elements ["A", "B", "C"]
+            , Var . MkFree . (<> fromString (show n)) <$>
+              elements ["a", "b", "c"]
+            ]
+          <*> vectorOf n arg
+
+spec :: Spec
+spec = do
+  describe "unify" do
+    -- NOTE: the danger here is that this one only ever succeeds when one of
+    -- the arguments is just a free variable. It might be better to
+    -- specifically generate types for which unification makes sense.
+    prop "unifies" \t u -> case unify t u of
+      Nothing -> discard
+      Just th -> subst th t == subst th u
+    prop "reflexive" \t -> isJust (unify t t)
+    -- NOTE: This does not check that the resulting unifications are
+    -- equivalent, which is quite tricky. This just checks symmetry if we look
+    -- at the result as boolean.
+    prop "symmetric" \t u -> case unify t u of
+      Nothing -> discard
+      Just _ -> isJust (unify u t)
